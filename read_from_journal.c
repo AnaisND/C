578da08bd9a1ee82fe047d6011c7458782d454b0
AnaisND <109735808+AnaisND@users.noreply.github.com>
2022-08-06 19:20:56 +0300

    Update read_from_journal.c

--- read_from_journal.c
+++ read_from_journal.c
@@ -1,37 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+
+
 typedef struct {
 	int day;
 	char date[11];
 	char content[1500];
-	char v;
 }Entry;
+
 
 typedef struct {
 	int password;
 }Cryptt;
 
-
-int numberr(FILE* f, int l)
-{
-	long p; int n;
-	p = ftell(f); 
-	fseek(f, 0, 2); 
-	n = ftell(f) / l;
-	fseek(f, p, 0); 
-	return n;
-}
-
 void main()
 {
-	FILE* f, *g;
+	FILE* f, * g;
 	Cryptt C;
 	Entry E;
 	int p;
-	int n = 0;
-	int day;
+	char buffer[2000];
 	printf("\nPassword: "); scanf("%d", &p);
-	printf("\nWhat day do you want to revisit? "); scanf("%d", &day);
 	f = fopen("crypt", "rb");
 	if (f == NULL)
 	{
@@ -39,26 +28,16 @@ void main()
 	}
 	else
 	{
-		g = fopen("myfile", "rb");
+		g = fopen("myfile", "r");
 		if (g == NULL)
 		{
 			printf("!ERROR!");
 		}
 		else
 		{
-			while (n < numberr(g, sizeof(Entry)))
+			while (fscanf(g, "%s", buffer) != EOF)
 			{
-				fseek(g, n * sizeof(Entry), 0);
-				fread(&E, sizeof(Entry), 1, g);
-				if ((E.v == 1) && (E.day == day))
-				{
-					printf("\nOn %s you wrote '%s' ", E.date, E.content);
-				}
-				else 
-				{
-					printf("\nNo entry found.");
-				}
-				n++;
+				printf("%s", buffer);
 			}
 		}
 		fclose(g);
