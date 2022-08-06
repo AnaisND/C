a2df3e710b8c033a67f069eec821aefdc0b9d8ec
AnaisND <109735808+AnaisND@users.noreply.github.com>
2022-08-06 18:34:10 +0300

    Update create.c

--- create.c
+++ create.c
@@ -1,10 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#include <stdio.h>
+
+
 typedef struct {
-	int day;
-	char* date;
+	char date[11];
 	char content[1500];
-	char v;
 }Entry;
 
 typedef struct {
@@ -14,8 +14,9 @@ typedef struct {
 
 void main()
 {
-	FILE* f, *g;
+	FILE* f, * g;
 	Cryptt C;
+	Entry E;
 	f = fopen("crypt", "wb");
 	if (f == NULL)
 	{
@@ -26,10 +27,16 @@ void main()
 		printf("\nCreate a password (numeric characters): ");
 		scanf("%d", &C.password);
 		fwrite(&C, sizeof(Cryptt), 1, f);
-		g = fopen("myfile", "wb");
+		g = fopen("myfile", "w");
 		if (g == NULL)
 		{
 			printf("!ERROR!");
+		}
+		else
+		{
+			printf("\nToday's date: "); getchar(); gets(E.date);
+			printf("\nThe content of your first entry (maximum 1500 characters): "); getchar(); gets(E.content);
+			fprintf(g, "\nDate: %s   %s", E.date, E.content);
 		}
 		fclose(g);
 	}
