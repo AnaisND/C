2552488f0c2cd637d38fcff128ca6aac80f6f08a
AnaisND <109735808+AnaisND@users.noreply.github.com>
2022-08-06 19:14:48 +0300

    Update add_content.c

--- add_content.c
+++ add_content.c
@@ -1,9 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+
+
 typedef struct {
+	int day;
 	char date[11];
 	char content[1500];
 }Entry;
+
 
 typedef struct {
 	int password;
@@ -13,8 +17,8 @@ typedef struct {
 void main()
 {
 	FILE* f, * g;
-	Cryptt C;
 	Entry E;
+	Cryptt C;
 	int p;
 	int n;
 	printf("\nPassword: "); scanf("%d", &p);
@@ -36,9 +40,10 @@ void main()
 			}
 			else
 			{
+				printf("\nHow many days have passed since having your diary? "); scanf("%d", E.day);
 				printf("\nToday's date: "); getchar(); gets(E.date);
 				printf("\nThe content of your entry: "); getchar(); gets(E.content);
-				fprintf(g, "\nDate: %s   %s", E.date, E.content);
+				fprintf(g, "\n%d Date: %s   '%s'", E.day, E.date, E.content);
 			}
 			fclose(g);
 		}
