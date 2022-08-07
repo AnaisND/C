532afa3b7a10aefa3f9ae512ce2f30bd4d973662
AnaisND <109735808+AnaisND@users.noreply.github.com>
2022-08-07 16:58:00 +0300

    Update read_from_journal.c

--- read_from_journal.c
+++ read_from_journal.c
@@ -1,11 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
-
 typedef struct {
 	int day;
 	char date[11];
 	char content[1500];
+	char v;
 }Entry;
 
 
@@ -13,34 +13,67 @@ typedef struct {
 	int password;
 }Cryptt;
 
+int L = sizeof(Entry);
+
+int entries(FILE* f, int l)
+{
+	long p; int n;
+	p = ftell(f);
+	fseek(f, 0, 2);
+	n = ftell(f) / l;
+	fseek(f, p, 0);
+	return n;
+}
+
+
 void main()
 {
-	FILE* f, * g;
+	FILE* f, * g, *h;
 	Cryptt C;
 	Entry E;
-	int p;
-	char buffer[2000];
+	int p, n;
 	printf("\nPassword: "); scanf("%d", &p);
 	f = fopen("crypt", "rb");
 	if (f == NULL)
 	{
-		printf("!ERROR!");
+		printf("\n!ERROR!");
 	}
-	else
+	else 
 	{
-		g = fopen("myfile", "r");
-		if (g == NULL)
+		rewind(f);
+		fread(&C, sizeof(Cryptt), 1, f);
+		if (p == C.password)
 		{
-			printf("!ERROR!");
-		}
-		else
-		{
-			while (fscanf(g, "%s", buffer) != EOF)
+			g = fopen("myfile", "rb+");
+			if (g == NULL)
 			{
-				printf("%s", buffer);
+				printf("\n!ERROR!");
 			}
+			else
+			{
+				h = fopen("memories", "w");
+				if (h == NULL)
+				{
+					printf("\n!ERROR!");
+				}
+				else
+				{
+					n = 0;
+					fread(&E, L, 1, g);
+					while (!feof(g))
+					{
+						if (E.v)
+						{
+							fprintf(h, "\n%s:   '%s'", E.date, E.content);
+						}
+						n++;
+						fread(&E, L, 1, g);
+					}
+				}
+				fclose(h);
+			}
+			fclose(g);
 		}
-		fclose(g);
 	}
 	fclose(f);
 }
