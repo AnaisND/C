0a9ebe8eb13caef920aa64ce602d3a405980e200
AnaisND <109735808+AnaisND@users.noreply.github.com>
2022-08-09 17:01:12 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+#include <time.h>
+#include "main.h"
+
+
+void main()
+{
+	hmainh();
+}
